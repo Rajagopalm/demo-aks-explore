3808c608d57fc8c001c8a12b1c9809205a791ae9
Rajagopal <rajagopalgms@outlook.com>
2026-01-17 19:26:32 +0530

    Comment out failing resources and outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -19,14 +19,14 @@ output "aks_kube_config" {
   sensitive = true
 }
 
-output "sp_client_id" {
-  value = azuread_application.github_actions.application_id
-}
+# output "sp_client_id" {
+#   value = azuread_application.github_actions.application_id
+# }
 
-output "sp_client_secret" {
-  value     = azuread_service_principal_password.github_actions.value
-  sensitive = true
-}
+# output "sp_client_secret" {
+#   value     = azuread_service_principal_password.github_actions.value
+#   sensitive = true
+# }
 
 output "sp_tenant_id" {
   value = data.azuread_client_config.current.tenant_id
